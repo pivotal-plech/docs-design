001653b25fa20195fffc80c101b672bd3bde0a74
Spencer Hurst <pair+spencer@pivotallabs.com>
2016-01-14 14:48:40 -0500

    search results

--- app/scripts/router.jsx
+++ app/scripts/router.jsx
@@ -7,12 +7,14 @@ var Layout = require('./components/layout');
 var Home = require('./components/home');
 var Product = require('./components/product/product');
 var Balancer = require('./components/balancer');
+var SearchResults = require('./components/searchResults');
 
 var routes = (
   <Route name="layout" path="/" handler={Layout}>
     <DefaultRoute handler={Home} />
     <Route handler={Product} name="/product" path="/product/:name" />
     <Route handler={Balancer} name="/balancer" />
+    <Route handler={SearchResults} name="/search-results" />
   </Route>
 );
 
